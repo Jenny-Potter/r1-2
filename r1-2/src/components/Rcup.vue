a370fe0358f9073e546d88b847d99bcc9e5c0865
王敏 <569049370@qq.com>
2020-04-07 18:58:42 +0800

    init project and havechange test

---
+++ r1-2/src/components/Rcup.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div>I am R cup components?have change</div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+    }
+  },
+
+  methods: {
+  },
+
+  created () {
+    // let app = getApp()
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ r1-2/src/pages/index/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div>I am r1-2 components project used by R-project</div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+    }
+  },
+
+  methods: {
+  },
+
+  created () {
+    // let app = getApp()
+  }
+}
+</script>
+
+<style scoped>
+</style>
